1fd992daf5d78c75a6b478bd2b4d4d49539548e9
tusharhero <tusharmaharana44676@gmail.com>
2022-12-17 20:42:49 +0530

    image printing function added.

--- asciirenderer.js
+++ asciirenderer.js
@@ -10,3 +10,23 @@ function createblankimg(x,y)
 	return img
 }
 
+function getstring(image)
+{
+	let string = ""
+	let leny = image.length
+	let lenx = image[0].length
+	for (let i = 0;i < leny;i++)
+	{
+		for (let j = 0;j < lenx;j++)
+		{
+			string += image[i][j]
+		}
+		string += "\n"
+	}
+	return string
+}
+
+function printimage(image)
+{
+	console.log(getstring(image))
+}
