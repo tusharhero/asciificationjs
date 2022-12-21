e1317cfe317f032e5d69b218719595a068d526bf
tusharhero <tusharmaharana44676@gmail.com>
2022-12-21 23:32:12 +0530

    added sliders for size and scale

--- main.mjs
+++ main.mjs
@@ -1,14 +1,19 @@
-import {getstring} from './asciirenderer.mjs'
+import {getstring,createblankimg} from './asciirenderer.mjs'
 import {plot} from './asciiplotter.mjs'
 
 const output = document.getElementById("out")
 const input = document.getElementById("in")
 const button = document.getElementById("mkplot")
+const size = document.getElementById("size")
+const scale = document.getElementById("scale")
 
 button.addEventListener("click",plotinout)
 
 function plotinout(){
 	var value = input.value
 	var ploted = plot(value)
-	output.innerHTML = getstring(ploted)
+	var sizeval = size.value
+	var scaleval = scale.value
+	console.log(sizeval,scaleval)
+	output.innerHTML = getstring(ploted,blankimg(sizeval,sizeval),undefined,undefined,scaleval)
 }
